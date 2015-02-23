80f98f8cbbecbea308ec98bed8e12e7d4880923c
Joel <pan.gamma.bizen@gmail.com>
2015-02-23 14:06:26 +0900

    128m 단위의 파일 생성으로 변경

--- drivers_telematics_analysis/src/parser.py
+++ drivers_telematics_analysis/src/parser.py
@@ -3,12 +3,25 @@
 __author__ = 'Joel Lee'
 
 import os
+import sys
 import math
+
+FILE_SIZE = 1024 * 1024 * 128
+
 
 def parse(src_dir, target_dir):
 
     # read driver directory list
     dlist = os.listdir(src_dir)
+
+    # create target directory
+    new_f_path = os.path.dirname(target_dir + "/")
+    if not os.path.exists(new_f_path):
+        os.makedirs(new_f_path)
+
+    #Initial variable
+    file_count = 0
+    record = ""
 
     for d in dlist:
         driver_dir = os.path.join(src_dir, d)
@@ -22,75 +35,82 @@ def parse(src_dir, target_dir):
 
                 trip_name = os.path.basename(os.path.splitext(f)[0])
 
-                trip_file = open(os.path.join(driver_dir,f),'r')
-                lines = trip_file.readlines()
+                try:
 
-                new_f_path = os.path.dirname(target_dir + "/" + driver_name + "/")
-                if not os.path.exists(new_f_path):
-                    os.makedirs(new_f_path)
+                    trip_file = open(os.path.join(driver_dir, f), 'r')
+                    lines = trip_file.readlines()
+                    trip_file.close()
 
-                output_file = open(new_f_path + "/" + trip_name + ".new", 'w')
-                trip_file.close()
+                    # output
+                    pre_loc = [0, 0]
+                    pre_velocity = 0
+                    seq = 0
 
-                # output
-                pre_loc = [0, 0]
-                pre_velocity = 0
-                seq = 0;
-                for line in lines[2:]:
+                    for line in lines[2:]:
 
-                    loc = line.split(',')
+                        if record == "":
+                            file_count += 1
+                            output_file = open(new_f_path + "/" + str(file_count) + ".csv", 'w')
 
-                    # 이동거리 계산
-                    x_delta =float( loc[0]) - pre_loc[0]
-                    y_delta = float(loc[1]) - pre_loc[1]
-                    distance = math.sqrt(pow(x_delta, 2) + pow(y_delta, 2))
+                        loc = line.split(',')
 
-                    # 속도 계산
-                    #  m/sec -> km/hour 변환
-                    velocity = distance * 3600 / 1000
+                        # 이동거리 계산
+                        x_delta = float(loc[0]) - pre_loc[0]
+                        y_delta = float(loc[1]) - pre_loc[1]
+                        distance = math.sqrt(pow(x_delta, 2) + pow(y_delta, 2))
 
-                    # 가속도 계산
-                    # ( 현재속도 - 이전속도 ) / 걸린 시간
-                    acceleration = velocity - pre_velocity
+                        # 속도 계산
+                        #  m/sec -> km/hour 변환
+                        velocity = distance * 3600 / 1000
 
-                    # 방향 계산
-                    # if (x_delta > 0 and y_delta > 0):
-                    #     news = 1
-                    # elif (x_delta < 0 and y_delta < 0):
-                    #     news = 2
-                    # elif (x_delta < 0 and y_delta < 0):
-                    #     news = 3
-                    # elif (x_delta > 0 and y_delta < 0):
-                    #     news = 4
+                        # 가속도 계산
+                        # ( 현재속도 - 이전속도 ) / 걸린 시간
+                        acceleration = velocity - pre_velocity
 
-                    # Trip Sequence
-                    seq = seq + 1
-                    accel_property = -1
+                        # 방향 계산
+                        # if (x_delta > 0 and y_delta > 0):
+                        #     news = 1
+                        # elif (x_delta < 0 and y_delta < 0):
+                        #     news = 2
+                        # elif (x_delta < 0 and y_delta < 0):
+                        #     news = 3
+                        # elif (x_delta > 0 and y_delta < 0):
+                        #     news = 4
 
-                    #가속 : 1, 감속:2, 정속: 3
-                    if(acceleration > 0):
-                        accel_property = 1
-                    elif(acceleration < 0):
-                        accel_property = 2
-                    elif(acceleration == 0):
-                        accel_property = 3
+                        # Trip Sequence
+                        seq += 1
+                        accel_property = -1
 
-                    # 운전자ID, 주행ID, 시퀀스, X, Y, 이동거리, 속도, 가속도, 가속도 구분
-                    record = driver_name + "," +  trip_name + "," + str(seq) + "," +  line.strip() + "," +str(distance) + ","  + str(velocity) + "," + str(acceleration) + "," + str(accel_property) + "\n"
-                    output_file.write(record)
+                        #가속 : 1, 감속:2, 정속: 3
+                        if acceleration > 0:
+                            accel_property = 1
+                        elif acceleration < 0:
+                            accel_property = 2
+                        elif acceleration == 0:
+                            accel_property = 3
 
-                    pre_loc = [float( loc[0]), float( loc[1])]
-                    pre_velocity = velocity
+                        # 운전자ID, 주행ID, 시퀀스, X, Y, 이동거리, 속도, 가속도, 가속도 구분
+                        record += driver_name + "," + trip_name + "," + str(seq) + "," + line.strip() + "," + str(
+                            distance) + "," + str(velocity) + "," + str(acceleration) + "," + str(accel_property) + "\n"
 
-                output_file.close()
+                        # 파일 생성
+                        if len(record) >= FILE_SIZE:
+                            output_file.write(record)
+                            output_file.close()
+                            record = ""
+
+                        pre_loc = [float(loc[0]), float(loc[1])]
+                        pre_velocity = velocity
+
+                except OSError as e:
+                    print e
 
 
-if __name__=="__main__":
-
-    src_dir = r"D:\10.study\Kaggle\drivers_telematics_analysis\drivers"
-    target_dir = r"D:\10.study\Kaggle\drivers_telematics_analysis\output"
-    # dir_path = sys.argv
-    parse(src_dir, target_dir)
+if __name__ == "__main__":
+    # src_dir = r"D:\10.study\Kaggle\drivers_telematics_analysis\drivers"
+    #target_dir = r"D:\10.study\Kaggle\drivers_telematics_analysis\output"
+    dir_path = sys.argv
+    parse(dir_path[1], dir_path[2])
 
 
 
