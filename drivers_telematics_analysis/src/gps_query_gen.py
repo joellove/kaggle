9aa9120791182f5c3a206d5d46a90b5767710f40
Joel <pan.gamma.bizen@gmail.com>
2016-01-13 16:29:53 +0900

    .

---
+++ drivers_telematics_analysis/src/gps_query_gen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# -*- coding:utf-8 -*-
+__author__ = 'Joel Lee'
+
+import os
+import sys
+import math
+
+def parse(src_file):
+    src_f = open(src_file, 'r')
+    lines = src_f.readlines()
+    src_f.close()
+
+    sql = 'case '
+    for line in lines[1:]:
+
+        loc = line.split(',')
+        sql += 'when latitude_fixed >= '  + str(float(loc[1]) - 0.0001) + ' and latitude_fixed <='   + str(float(loc[1]) + 0.0001) + ' and longitude_fixed >=' + str( float(loc[2]) - 0.0001) + ' and longitude_fixed <= ' + str(float(loc[2]) + 0.0001) + " then '" + loc[0].strip() + "' \n"
+    sql += 'end as point'
+    print(sql)
+
+if __name__ == "__main__":
+    src_file = r"D:\svc_touchpoint_seoul.code.latitude.longitude.csv"
+    #dir_path = sys.argv
+    parse(src_file)
+
